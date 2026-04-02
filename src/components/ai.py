02e37e93a1e375d78cd0ed93c49ae176cfb8845a
Bo Qin <arrayofchar@gmail.com>
2026-04-01 19:13:07 -0700

    bug fixed

--- src/components/ai.py
+++ src/components/ai.py
@@ -262,13 +262,14 @@ class CritterAI(BaseAI):
                 self.path = self.get_path_to(*self.plants[self.plants_index][0])
                 self.plants_index = (self.plants_index + 1) % len(self.plants)
             self.last_hp = self.entity.fighter.hp
-            # if self.idle_tick > consts.CRITTER_HIDE_THRESHOLD:
-            #     self.idle_tick = 0
-            #     tiles = []
-            #     for t in self.entity.gamemap.get_neighbor_tiles(z, x, y):
-            #         if self.entity.gamemap.tiles["tile_type"][*t] == tile_types.TileType.FLOOR:
-            #             tiles.append(t)
-            #     return MovementAction(self.entity, *tiles[random.randint(0, len(tiles) - 1)]).perform()
+            if self.idle_tick > consts.CRITTER_HIDE_THRESHOLD:
+                self.idle_tick = 0
+                tiles = []
+                for t in self.entity.gamemap.get_neighbor_tiles(z, x, y):
+                    if self.entity.gamemap.tiles["tile_type"][*t] == tile_types.TileType.FLOOR:
+                        tiles.append(t)
+                dest_z, dest_x, dest_y = random.choice(tiles)
+                return MovementAction(self.entity, dest_z - self.entity.z, dest_x - self.entity.x, dest_y - self.entity.y).perform()
         if self.path:
             dest_z, dest_x, dest_y = self.path.pop(0)
             if self.path:
